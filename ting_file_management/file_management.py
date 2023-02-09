e3ec383bf859018d1af39859f03598b6bbb22076
Aline Soliman <alineapreis@hotmail.com>
2023-02-09 20:13:18 -0300

    feat: implementa função txt_importer dentro do módulo file_management capaz de importar notícias a partir de um arquivo TXT

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,14 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not path_file.endswith(".txt"):
+        return print(ValueError("Formato inválido"), file=sys.stderr)
+
+    try:
+        with open(path_file, mode='r') as file:
+            result = file.read().split('\n')
+            return result
+
+    except FileNotFoundError:
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
